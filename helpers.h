995c4d32cdcb4720d11878e210f70bdf098f3885
adampower48 <adam.power48@mail.dcu.ie>
2021-01-03 18:43:28 +0000

    Add vector printing helper.

--- helpers.h
+++ helpers.h
@@ -1,2 +1,16 @@
 #pragma once
 #include <string>
+#include <vector>
+
+
+template <typename T>
+std::string strVector(std::vector<T> vec){
+	std::string out = "(";
+
+	for (size_t i = 0; i < vec.size() - 1; ++i) {
+		out += std::to_string(vec[i]) + ", ";
+	}
+	out += std::to_string(vec.back()) + ")";
+
+	return out;
+}
